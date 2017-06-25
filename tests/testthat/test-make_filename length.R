3b609b25a32bdeeeb6a7a6ee204d4e12919ccea8
Josh Zirin <jzirin3885@gmail.com>
2017-06-24 19:56:03 -0500

    more portability of test that issues

--- tests/testthat/test-make_filename length.R
+++ tests/testthat/test-make_filename length.R
@@ -1,5 +1,5 @@
-context("make_filename length")
+testthat::context("make_filename length")
 
 testthat::test_that("make_filename has right length", {
-  testthat::expect_that(nchar(make_filename(2017)), equals(21))
+  testthat::expect_equal(nchar(make_filename(2017)),21)
 })
